ca53dff34aa3d1f2e3c26ac628cdfb4610c069b0
Simon Cooper <cooper.sf@gmail.com>
2019-01-19 11:52:56 +0000

    adding sendgrid emailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -60,4 +62,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+    # change to true to allow email to be sent during development
+    config.action_mailer.perform_deliveries = true
+
+    config.action_mailer.smtp_settings = {
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :domain => 'thegd.herokuapp.com',
+        :address => 'smtp.sendgrid.net',
+        :port => 587,
+        :authentication => :plain,
+        :enable_starttls_auto => true
+      }
 end
