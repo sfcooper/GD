7d76e022b9b3ebeac15810590990f24f32c54db0
Simon Cooper <cooper.sf@gmail.com>
2019-01-20 15:37:53 +0000

    removing packs from gitignore

---
+++ public/packs/application-91e3829d8cecf145aadf.js
@@ -0,0 +1,105 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/packs/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 4:
+/*!*********************************************!*\
+  !*** ./app/javascript/packs/application.js ***!
+  \*********************************************/
+/*! no exports provided */
+/*! all exports used */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__css_application_css__ = __webpack_require__(/*! ../css/application.css */ 5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__css_application_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__css_application_css__);
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+console.log('Hello World from Webpacker');
+
+/***/ }),
+
+/***/ 5:
+/*!********************************************!*\
+  !*** ./app/javascript/css/application.css ***!
+  \********************************************/
+/*! dynamic exports provided */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: Loading PostCSS Plugin failed: Specified Tailwind config file \"/app/javascript/css/tailwind.js\" doesn't exist.\n\n(@/Users/Simon/Sites/GD/.postcssrc.yml)\n    at load (/Users/Simon/Sites/GD/node_modules/postcss-load-config/src/plugins.js:27:13)\n    at Object.keys.filter.map (/Users/Simon/Sites/GD/node_modules/postcss-load-config/src/plugins.js:53:16)\n    at Array.map (<anonymous>)\n    at plugins (/Users/Simon/Sites/GD/node_modules/postcss-load-config/src/plugins.js:52:8)\n    at config.load.then (/Users/Simon/Sites/GD/node_modules/postcss-load-config/src/index.js:72:18)\n    at runLoaders (/Users/Simon/Sites/GD/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/Simon/Sites/GD/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/Simon/Sites/GD/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/Simon/Sites/GD/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Promise.resolve.then.then.catch (/Users/Simon/Sites/GD/node_modules/postcss-loader/lib/index.js:194:71)");
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=application-91e3829d8cecf145aadf.js.map
